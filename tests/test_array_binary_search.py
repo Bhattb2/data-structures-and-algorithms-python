62f3971ef49577e3597de85a37914332329f159c
Bhagirath Bhatt <budbhatt@gmail.com>
2020-12-16 15:33:16 -0800

    fixed test issues and more

---
+++ tests/test_array_binary_search.py
@@ -0,0 +1,7 @@
+from dsa.challenges.array_binary_search import __version__
+from dsa.challenges.array_binary_search.array_binary_search import BinarySearch
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
