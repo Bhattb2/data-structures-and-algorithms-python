432595e9b3cd7e0348ea8296fdf73834543dafe4
Bhagirath Bhatt <budbhatt@gmail.com>
2020-12-20 17:26:56 -0800

    some tests still failing

--- dsa/challenges/linked-list/tests/test_linked_list.py
+++ dsa/challenges/linked-list/tests/test_linked_list.py
@@ -1,11 +1,47 @@
-from linked_list import Node, __version__
-# from linked_list import Node, LinkedList
-# import linked_list
+from linked_list import __version__
+from linked_list.linked_list import Node, LinkedList
 
 def test_version():
     assert __version__ == '0.1.0'
 
-
 def test_node_exist():
-    node = Node()
-    assert node.version == 1
+    assert Node
+
+def test_LinkedList_exist():
+    assert LinkedList
+
+def test_empty_list():
+    ll = LinkedList()
+    assert ll
+
+def test_insert_to_empty_list():
+    ll = LinkedList()
+    ll.insert('Sunday')
+    assert ll.head.value == 'Sunday'
+
+def test_insert_multiple_to_empty_list():
+    ll = LinkedList()
+    ll.insert('Tuesday')
+    ll.insert('Monday')
+    ll.insert('Sunday')
+    assert ll.head.value == 'Sunday'
+
+def test_next_node_value():
+    ll = LinkedList()
+    ll.insert('Tuesday')
+    ll.insert('Monday')
+    ll.insert('Sunday')
+    assert ll.head.next_node.value == 'Monday'
+
+def test_includes_value_true():
+    ll = LinkedList()
+    ll.includes('Monday')
+    assert True
+
+def test_include_value_false():
+    ll = LinkedList
+    actual = ll.includes('November')
+    expected = False
+    assert actual == expected
+
+
